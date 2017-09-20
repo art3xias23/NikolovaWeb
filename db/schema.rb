0654797281f807b84bec8a328f033282ab55a3e0
art3xias23 <k.milchev.12@aberdeen.ac.uk>
2017-09-20 22:57:02 +0100

    migration rollback

--- db/schema.rb
+++ db/schema.rb
@@ -21,4 +21,11 @@ ActiveRecord::Schema.define(version: 20170920131113) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
